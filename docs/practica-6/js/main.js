517fa933eb462f4e0146258c14727249a476cc96
LuisLYiztino <luiselp.080972@gmail.com>
2024-09-26 17:58:14 -0600

    Agregando practica 6 Simulador de compra en html

---
+++ docs/practica-6/js/main.js
@@ -0,0 +1,6 @@
+import { mostrarProductosDisponibles } from "./components/practica6.js";
+import { agregarOquitarDelCarrito } from "./components/practica6.js";
+import { comprar } from "./components/practica6.js";
+mostrarProductosDisponibles();
+agregarOquitarDelCarrito();
+comprar();
